0df6305ab2f87494d44f8dc20b791100625c2bf4
andhikagama <agandhika@gmail.com>
2022-02-12 00:13:33 +0700

    find possible treasure location

--- main.go
+++ main.go
@@ -5,29 +5,25 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strings"
 )
 
-func main() {
-	text := getTextFromFile("case.txt")
-
-	matrix := make([][]string, len(text))
-
-	fmt.Println("case layout : ")
-	for i, line := range text {
-		chars := []rune(line)
-		strs := make([]string, len(chars))
-
-		for j, v := range chars {
-			str := string(v)
-			strs[j] = str
-		}
-
-		matrix[i] = strs
-		fmt.Println(line)
-	}
+type dungeon struct {
+	text   []string
+	matrix [][]string
+	row    int
+	col    int
+	startX int
+	startY int
 }
 
-func getTextFromFile(filename string) []string {
+func main() {
+	d := newDungeon("case.txt")
+	d.buildMatrix()
+	d.findPossibleTreasureLocation()
+}
+
+func newDungeon(filename string) dungeon {
 	var text []string
 
 	file, err := os.Open(filename)
@@ -48,5 +44,69 @@ func getTextFromFile(filename string) []string {
 	}
 
 	file.Close()
-	return text
+
+	return dungeon{
+		text: text,
+		row:  len(text),
+		col:  len(text[0]),
+	}
+}
+
+func (d *dungeon) buildMatrix() {
+	fmt.Println()
+	fmt.Println("dungeon layout : ")
+
+	matrix := make([][]string, len(d.text))
+	for i, line := range d.text {
+		chars := []rune(line)
+		strs := make([]string, len(chars))
+
+		for j, v := range chars {
+			str := string(v)
+			strs[j] = str
+
+			if strings.ToLower(str) == "x" {
+				d.startX = i
+				d.startY = j
+			}
+		}
+
+		matrix[i] = strs
+		fmt.Println(line)
+	}
+
+	d.matrix = matrix
+}
+
+func (d *dungeon) findPossibleTreasureLocation() {
+	fmt.Println()
+	fmt.Println("list possible treasure index location :")
+
+	// rules: up > right > down
+	for i := d.startX - 1; i > 0; i-- {
+		for j := d.startY + 1; j < d.col; j++ {
+			if d.matrix[i][j] == "#" {
+				continue
+			}
+
+			for k := i + 1; k < d.row; k++ {
+				if d.matrix[k][j] == "#" {
+					break
+				}
+
+				fmt.Println(fmt.Sprintf("[%d,%d]", k, j))
+				d.matrix[k][j] = "$"
+			}
+
+		}
+	}
+
+	fmt.Println()
+	fmt.Println("marked possible treasure location in dungeon :")
+
+	for _, v := range d.matrix {
+		fmt.Println(strings.Join(v, ""))
+	}
+
+	fmt.Println()
 }
